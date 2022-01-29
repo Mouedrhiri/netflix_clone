0630ff358830789b83f9b985f737cd67e0b44f2d
Mohammed Ouedrhiri <mouedrhiri492@gmail.com>
2022-01-29 17:10:07 +0100

    Creation of Netflix Clone using React js

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import "./App.css";
+import requests from "./requests";
+import Row from "./Row";
+import Banner from "./Banner";
+import Nav from "./Nav";
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+      <Banner />
+      <Row
+        title="Netflix Originals"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+      <Row title="Trending" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
+      <footer
+        style={{
+          alignContent: "center",
+        }}
+      >
+        Developped By Mohammed Ouedrhiri 2022
+      </footer>
+    </div>
+  );
+}
+
+export default App;
